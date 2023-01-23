089b469a0781ecd1799bd2091e0bb59af3240109
Rilwan <rilwan.at@gmail.com>
2023-01-23 17:14:30 +0100

    first commit

---
+++ Numbers/Form3.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Numbers
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.txt_q = new System.Windows.Forms.TextBox();
+            this.txt_a = new System.Windows.Forms.TextBox();
+            this.btn_s = new System.Windows.Forms.Button();
+            this.txt_inf = new System.Windows.Forms.TextBox();
+            this.btn_gen = new System.Windows.Forms.Button();
+            this.txt_command = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txt_ca = new System.Windows.Forms.TextBox();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.btnCalculator = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_q
+            // 
+            this.txt_q.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_q.Font = new System.Drawing.Font("NewsGoth Cn BT", 69.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_q.Location = new System.Drawing.Point(12, 202);
+            this.txt_q.Name = "txt_q";
+            this.txt_q.ReadOnly = true;
+            this.txt_q.Size = new System.Drawing.Size(560, 112);
+            this.txt_q.TabIndex = 0;
+            this.txt_q.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txt_a
+            // 
+            this.txt_a.Font = new System.Drawing.Font("NewsGoth Cn BT", 69.75F);
+            this.txt_a.Location = new System.Drawing.Point(396, 320);
+            this.txt_a.MaxLength = 4;
+            this.txt_a.Name = "txt_a";
+            this.txt_a.Size = new System.Drawing.Size(176, 119);
+            this.txt_a.TabIndex = 1;
+            this.txt_a.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn_s
+            // 
+            this.btn_s.Location = new System.Drawing.Point(464, 12);
+            this.btn_s.Name = "btn_s";
+            this.btn_s.Size = new System.Drawing.Size(1, 1);
+            this.btn_s.TabIndex = 2;
+            this.btn_s.UseVisualStyleBackColor = true;
+            this.btn_s.Click += new System.EventHandler(this.btn_s_Click);
+            // 
+            // txt_inf
+            // 
+            this.txt_inf.BackColor = System.Drawing.SystemColors.Control;
+            this.txt_inf.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_inf.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_inf.Location = new System.Drawing.Point(212, 131);
+            this.txt_inf.Multiline = true;
+            this.txt_inf.Name = "txt_inf";
+            this.txt_inf.ReadOnly = true;
+            this.txt_inf.Size = new System.Drawing.Size(360, 26);
+            this.txt_inf.TabIndex = 3;
+            this.txt_inf.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn_gen
+            // 
+            this.btn_gen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_gen.Location = new System.Drawing.Point(464, 65);
+            this.btn_gen.Name = "btn_gen";
+            this.btn_gen.Size = new System.Drawing.Size(108, 26);
+            this.btn_gen.TabIndex = 4;
+            this.btn_gen.Text = "Begin";
+            this.btn_gen.UseVisualStyleBackColor = true;
+            this.btn_gen.Click += new System.EventHandler(this.btn_gen_Click);
+            // 
+            // txt_command
+            // 
+            this.txt_command.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_command.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_command.Location = new System.Drawing.Point(212, 163);
+            this.txt_command.Multiline = true;
+            this.txt_command.Name = "txt_command";
+            this.txt_command.ReadOnly = true;
+            this.txt_command.Size = new System.Drawing.Size(360, 26);
+            this.txt_command.TabIndex = 5;
+            this.txt_command.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 526);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(560, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txt_ca
+            // 
+            this.txt_ca.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_ca.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_ca.Location = new System.Drawing.Point(212, 445);
+            this.txt_ca.Multiline = true;
+            this.txt_ca.Name = "txt_ca";
+            this.txt_ca.ReadOnly = true;
+            this.txt_ca.Size = new System.Drawing.Size(360, 75);
+            this.txt_ca.TabIndex = 7;
+            this.txt_ca.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.BackColor = System.Drawing.Color.White;
+            this.btnAbout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAbout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAbout.Location = new System.Drawing.Point(12, 12);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(25, 25);
+            this.btnAbout.TabIndex = 13;
+            this.btnAbout.Text = "?";
+            this.btnAbout.UseVisualStyleBackColor = false;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // btnCalculator
+            // 
+            this.btnCalculator.BackColor = System.Drawing.Color.White;
+            this.btnCalculator.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCalculator.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCalculator.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalculator.Location = new System.Drawing.Point(43, 12);
+            this.btnCalculator.Name = "btnCalculator";
+            this.btnCalculator.Size = new System.Drawing.Size(25, 25);
+            this.btnCalculator.TabIndex = 14;
+            this.btnCalculator.UseVisualStyleBackColor = false;
+            this.btnCalculator.Click += new System.EventHandler(this.btnCalculator_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(74, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 25);
+            this.button2.TabIndex = 15;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Enabled = false;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(105, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 25);
+            this.button3.TabIndex = 16;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(136, 12);
+            this.trackBar1.Maximum = 7;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(322, 45);
+            this.trackBar1.TabIndex = 17;
+            this.trackBar1.Visible = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCalculator);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.txt_ca);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txt_command);
+            this.Controls.Add(this.btn_gen);
+            this.Controls.Add(this.txt_inf);
+            this.Controls.Add(this.btn_s);
+            this.Controls.Add(this.txt_a);
+            this.Controls.Add(this.txt_q);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_q;
+        private System.Windows.Forms.TextBox txt_a;
+        private System.Windows.Forms.Button btn_s;
+        private System.Windows.Forms.TextBox txt_inf;
+        private System.Windows.Forms.Button btn_gen;
+        private System.Windows.Forms.TextBox txt_command;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txt_ca;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button btnCalculator;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
---
+++ Numbers/Form3.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numbers
+{
+    public partial class Form3 : Form
+    {
+        int right, wrong = 0;
+        string ans;
+
+        string[] coefa = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+        string coef = "";
+        string alg_z = "";
+
+        int rn = -1;
+
+        Form formb = new Form();
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowBackForm()
+        {
+            formb = new Form();
+            formb.Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height - 2);
+            //formb.BackColor = //FromArgb(255, 79, 254, 27);
+            formb.MaximizeBox = false;
+            formb.MinimizeBox = false;
+            formb.ControlBox = false;
+            formb.FormBorderStyle = FormBorderStyle.Fixed3D;
+            //formb.Icon = 
+            //formb.Icon = Numbers.Properties.Resources.cl_icon;
+            formb.SendToBack();
+
+            formb.Click += new EventHandler(formb_Click);
+            formb.Show();
+        }
+
+        void formb_Click(object sender, EventArgs e)
+        {
+            formb.BringToFront();
+            this.BringToFront();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            ShowBackForm();
+
+            //this.Text = "Algebra - " + "by rilobas applications (www.rilobas.app)";
+            this.Text = "Learn Algebra";
+            txt_inf.Text = "Click Begin, type in the answer, then Press Enter...";
+
+            AcceptButton = btn_s;
+        }
+
+        private void btn_s_Click(object sender, EventArgs e)
+        {
+            if (txt_q.Text != "")
+            {
+                MarkQuestion();
+            }
+        }
+
+        private void MarkQuestion()
+        {
+
+            //MessageBox.Show(rn.ToString());
+            //System.IO.File.AppendAllText(@"C://Users/" + Environment.UserName + "/Desktop/sols.txt", txt_q.Text + " --> " + txt_a.Text + Environment.NewLine);
+
+            if (rn == 0)
+            {
+                if (txt_a.Text == ans.ToString())
+                {
+                    //MessageBox.Show("Correct!");
+                    right++;
+                    System.Media.SystemSounds.Asterisk.Play();
+                }
+                else
+                {
+                    wrong++;
+                    //MessageBox.Show(ans.ToString());               
+                    System.Media.SystemSounds.Beep.Play();
+                }
+            }
+            else
+            {
+                if (txt_a.Text == ans.ToString())
+                {
+                    //MessageBox.Show("Correct!");
+                    right++;
+                    System.Media.SystemSounds.Asterisk.Play();
+                }
+                else
+                {
+                    wrong++;
+                    //MessageBox.Show(ans.ToString());               
+                    System.Media.SystemSounds.Beep.Play();
+                }
+            }
+            btn_gen.PerformClick();
+        }
+
+        private void btn_gen_Click(object sender, EventArgs e)
+        {
+            txt_a.ReadOnly = false;
+
+            progressBar1.Value = 100;
+            timer1.Start();
+
+            txt_ca.Text = "";
+            txt_a.Text = "";
+            double pscore = (double.Parse(right.ToString()) / double.Parse((right + wrong).ToString()) * 100);
+            txt_inf.Text = "score: " + right.ToString() + " / " + (right + wrong).ToString() + " (" + Math.Round(pscore, 2) + "%)";
+
+
+            GetTopic();
+
+            txt_a.Focus();
+
+
+        }
+
+        private void GetTopic()
+        {
+            Random rnd = new Random(DateTime.Now.Millisecond);
+
+
+            rn = rnd.Next(0, 2);
+
+
+            if (rn == 0)//Algebra
+            {
+                int n1 = rnd.Next(-20, 11);
+                int n2 = rnd.Next(-20, 11);
+                ans = (n1 + n2).ToString();
+
+                coef = coefa[rnd.Next(0, 26)];
+
+
+                string sn1 = n1.ToString();
+                string sn2 = n2.ToString();
+
+
+                if (sn2.Substring(0, 1) == "-")
+                {
+
+                }
+                else
+                {
+                    sn2 = "+" + sn2;
+                }
+
+                string[] qqs = new string[3] { "?", "??", "???" };
+                string qq = qqs[rnd.Next(0, 2)];
+
+                txt_command.Text = "Solve the algebraic equation";
+                txt_q.Text = "" + sn1 + coef + "" + sn2 + coef + " = " + qq;
+
+                if (ans == "-1") { ans = "-" + coef; }
+                else if (ans == "1") { ans = coef; }
+                else if (ans == "0")
+                {
+                    //ans = ans;
+                }
+                else { ans = ans + coef; }
+            }
+            else if (rn == 1)
+            {
+                algebra();
+                //
+            }
+
+            //this.Text = ans;
+
+        }
+
+        private void algebra()
+        {
+            coef = "";
+
+            Random alge = new Random(DateTime.Now.Millisecond);
+            int alg_w, alg_x, alg_y = 0;
+
+
+            alg_w = alge.Next(1, 10);
+            alg_x = alge.Next(1, 10);
+            alg_y = alge.Next(1, 10);
+
+            alg_z = coefa[alge.Next(0, 26)];
+
+
+            int q_switch = alge.Next(0, 3);
+            switch (q_switch)
+            {
+                case (0):
+                    txt_command.Text = "Solve for " + alg_z + " in the equation";
+                    break;
+                case (1):
+                    txt_command.Text = "Find " + alg_z + " in the equation";
+                    break;
+                case (2):
+                    txt_command.Text = "What is the value of " + alg_z + " in the equation?";
+                    break;
+            }
+
+            if (DateTime.Now.Millisecond % 3 == 0)
+            {
+                txt_q.Text = "" + alg_w + alg_z + "=" + alg_w * alg_y + "";
+                ans = alg_y.ToString();
+            }
+            else if (DateTime.Now.Millisecond % 3 == 1)
+            {
+                int alg_k = (alg_w * alg_y) + (alg_w * alg_x);
+                txt_q.Text = "" + alg_w + alg_z + "+" + alg_w * alg_y + "=" + alg_k + "";
+                ans = ((((alg_w * alg_y) + (alg_w * alg_x)) - (alg_w * alg_y)) / alg_w).ToString();
+            }
+            else if (DateTime.Now.Millisecond % 3 == 2)
+            {
+                int alg_k = (alg_w * alg_y) + (alg_w * alg_x);
+                txt_q.Text = "" + alg_w + alg_z + "-" + alg_w * alg_y + "=" + alg_k + "";
+                ans = ((((alg_w * alg_y) + (alg_w * alg_x)) + (alg_w * alg_y)) / alg_w).ToString();
+            }
+
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string tu = "Time UP! - ";
+            progressBar1.Value = progressBar1.Value - 5;
+            if (progressBar1.Value == 0)
+            {
+                txt_a.ReadOnly = true;
+                timer1.Stop();
+
+                if (txt_a.Text == ans.ToString())
+                {
+
+                    btn_s_Click(sender, e);
+                }
+                else
+                {
+                    if (coef == "")
+                    {
+                        txt_ca.Text = tu + "The correct value for " + alg_z + " is " + ans.ToString();
+                    }
+                    else
+                    {
+                        txt_ca.Text = tu + "The correct answer is " + ans.ToString();
+                    }
+
+                    txt_ca.Text += Environment.NewLine + "Press Enter to continue..";
+                }
+
+
+            }
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.rbapps.app/product-alg.php");
+            btn_gen.Focus();
+        }
+
+        private void btnCalculator_Click(object sender, EventArgs e)
+        {
+            
+            System.Diagnostics.Process.Start("calc.exe");
+            btnCalculator.Visible = false;
+        }
+
+
+    }
+}
